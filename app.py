7fb0048e75671c2be231694fb92202fdd80fc7b7
Adilet Gaparov <adilet@capchase.com>
2024-10-24 01:14:36 +0200

    fix

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def pull_attendance_data(fetching_date: str):
     }
 
 def get_rating_chart(min_score, max_score, student_score, student_rank, total_participants):
-    leader_text = 'Лидер'
+    leader_text = 'Лидер #1'
     student_rating_text = f'Ваш рейтинг #{int(student_rank)}'
     last_place_text = f'Последнее место #{total_participants}'
     chart_data = pd.DataFrame({
