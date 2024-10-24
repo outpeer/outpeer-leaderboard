c482d9326a1a8ec5946c5f3ce7e5cbec90739af5
Adilet Gaparov <adilet@capchase.com>
2024-10-24 03:28:48 +0200

    chore

--- app.py
+++ app.py
@@ -31,12 +31,12 @@ def pull_data(data_type: str, fetching_date: str):
     }
 
 def get_rating_chart(min_score, max_score, student_score, student_rank, total_participants):
-    chart_data = pd.DataFrame({
+    data = pd.DataFrame({
         'Category': ['Лидер #1', f'Ваш рейтинг #{int(student_rank)}', f'Последнее место #{total_participants}'],
         'Score': [max_score, student_score, min_score],
     })
     
-    fig = px.bar(chart_data.sort_values('Score', ascending=True), 
+    fig = px.bar(data.sort_values('Score', ascending=True), 
                  y='Category', x='Score', orientation='h',
                  title='Ваше место в рейтинге',
                  labels={'Score': 'Балл', 'Category': ''},
@@ -60,32 +60,32 @@ def display_student_info(student_leaderboard_df):
 def display_rating_chart(leaderboard_df, student_leaderboard_df):
     max_score = leaderboard_df["Total score"].max()
     min_score = leaderboard_df["Total score"].min()
-    student_score = student_leaderboard_df["Total score"].values[0]
-    student_rank = student_leaderboard_df["Рейтинг"].values[0]
-    rating_chart = get_rating_chart(min_score, max_score, student_score, student_rank, len(leaderboard_df))
-    st.plotly_chart(rating_chart)
+    score = student_leaderboard_df["Total score"].values[0]
+    rank = student_leaderboard_df["Рейтинг"].values[0]
+    chart = get_rating_chart(min_score, max_score, score, rank, len(leaderboard_df))
+    st.plotly_chart(chart)
 
 def display_homework_chart(homework_df, student_id):
     hw_columns = [col for col in homework_df.columns if col.startswith("HW")]
-    relevant_homework_df = homework_df[hw_columns].dropna(axis=1, how="all")
-    count_homeworks = len(relevant_homework_df.columns)
+    completed_homework_df = homework_df[hw_columns].dropna(axis=1, how="all")
+    count_homeworks = len(completed_homework_df.columns)
     
-    hw_avg_score_class = (relevant_homework_df.sum(axis=1) / count_homeworks).mean()
-    hw_avg_scores = relevant_homework_df.mean(axis=0).values
-    hw_avg_scores = [*hw_avg_scores, *[None] * (len(hw_columns) - len(hw_avg_scores))]
+    avg_score_across_class = (completed_homework_df.sum(axis=1) / count_homeworks).mean()
+    avg_score_per_homework = completed_homework_df.mean(axis=0).values
+    avg_score_per_homework = [*avg_score_per_homework, *[None] * (len(hw_columns) - len(avg_score_per_homework))]
 
     student_homework_df = homework_df[homework_df["ИИН"] == student_id]
-    hw_labels = [str(col[2:]) for col in hw_columns]
-    hw_scores = student_homework_df[hw_columns].iloc[0].values
-    hw_avg_score = sum([score for score in hw_scores if not pd.isna(score)]) / count_homeworks
+    labels = [str(col[2:]) for col in hw_columns]
+    scores = student_homework_df[hw_columns].iloc[0].values
+    avg_score = sum([score for score in scores if not pd.isna(score)]) / count_homeworks
 
-    hw_data = pd.concat([
-        pd.DataFrame({"labels": hw_labels, "scores": hw_avg_scores, "type": ["Класс"] * len(hw_labels)}),
-        pd.DataFrame({"labels": hw_labels, "scores": hw_scores, "type": ["Ваш балл"] * len(hw_labels)})
+    data = pd.concat([
+        pd.DataFrame({"labels": labels, "scores": avg_score_per_homework, "type": ["Класс"] * len(labels)}),
+        pd.DataFrame({"labels": labels, "scores": scores, "type": ["Ваш балл"] * len(labels)})
     ])
 
-    hw_chart = px.bar(
-        hw_data,
+    chart = px.bar(
+        data,
         x="labels", y="scores", color="type",
         color_discrete_map={"Ваш балл": "blue", "Класс": "lightgrey"},
         category_orders={"type": ["Класс", "Ваш балл"]},
@@ -93,52 +93,51 @@ def display_homework_chart(homework_df, student_id):
         title="Ваши домашние задания",
         barmode="group",
     )
-    hw_chart.update_layout(yaxis_range=[0, 100])
-    hw_chart.add_hline(
-        y=hw_avg_score,
+    chart.update_layout(yaxis_range=[0, 100])
+    chart.add_hline(
+        y=avg_score,
         line_dash="dash",
-        line_color="red" if hw_avg_score < hw_avg_score_class else "green",
-        annotation_text=f"Ваш средний балл: {hw_avg_score:.2f}",
+        line_color="red" if avg_score < avg_score_across_class else "green",
+        annotation_text=f"Ваш средний балл: {avg_score:.2f}",
         annotation_position="top right",
     )
-    hw_chart.add_hline(
-        y=hw_avg_score_class,
+    chart.add_hline(
+        y=avg_score_across_class,
         line_dash="dash",
         line_color="black",
-        annotation_text=f"Средний балл класса: {hw_avg_score_class:.2f}",
+        annotation_text=f"Средний балл класса: {avg_score_across_class:.2f}",
         annotation_position="top right",
     )
-    st.plotly_chart(hw_chart)
+    st.plotly_chart(chart)
 
 def display_attendance_chart(attendance_df, student_id):
-    col_dates_start_index = attendance_df.columns.get_loc("Week 1")
+    dates_start_index = attendance_df.columns.get_loc("Week 1")
     student_attendance_df = attendance_df[attendance_df["ИИН"] == student_id]
-    lesson_dates = attendance_df.iloc[0][col_dates_start_index:]
+    lesson_dates = attendance_df.iloc[0][dates_start_index:]
     lesson_dates = [datetime.strptime(date, "%d.%m.%y") for date in lesson_dates[lesson_dates.notna()].tolist()]
-    total_lessons = len(lesson_dates)
-    attendance_scores = student_attendance_df.iloc[0][col_dates_start_index:total_lessons+col_dates_start_index+1].tolist()
+    scores = student_attendance_df.iloc[0][dates_start_index:dates_start_index+len(lesson_dates)].tolist()
 
-    attendance_data = pd.DataFrame({
+    data = pd.DataFrame({
         "dates": lesson_dates,
-        "scores": attendance_scores,
+        "scores": scores,
     })
-    attendance_avg_score = sum([score for score in attendance_data["scores"] if not pd.isna(score)]) / len(attendance_data)
+    avg_score = sum([score for score in scores if not pd.isna(score)]) / len(scores)
 
-    attendance_chart = px.bar(
-        attendance_data,
+    chart = px.bar(
+        data,
         x="dates", y="scores",
         labels={"dates": "Даты уроков", "scores": ""},
         title="Ваша посещаемость",
     )
-    attendance_chart.update_layout(yaxis_range=[0, 1.1])
-    attendance_chart.add_hline(
-        y=attendance_avg_score,
+    chart.update_layout(yaxis_range=[0, 1.1])
+    chart.add_hline(
+        y=avg_score,
         line_dash="solid",
         line_color="red",
-        annotation_text=f"Средняя посещаемость: {100 * attendance_avg_score:.0f}%",
+        annotation_text=f"Средняя посещаемость: {100 * avg_score:.0f}%",
         annotation_position="top right",
     )
-    st.plotly_chart(attendance_chart)
+    st.plotly_chart(chart)
 
 # Main
 course = st.selectbox(
